f7d8a9bb4fcbc76def5161b0e6ed9c846e195e39
ilyamishkin <iluhamishkin@gmail.com>
2019-11-25 16:28:37 +0100

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanetListComponent } from './planet-list/planet-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetListComponent
   ],
   imports: [
     BrowserModule,
