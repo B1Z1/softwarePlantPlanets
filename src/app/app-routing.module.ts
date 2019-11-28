dcf5517d10b9f971a1515928dda4a71fe82c63fd
ilyamishkin <iluhamishkin@gmail.com>
2019-11-28 16:42:08 +0100

    Created new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 
+import { PlanetComponent } from './pages/planet/planet.component'
+import { MainComponent } from './pages/main/main.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent
+  },
+  {
+    path: ':name',
+    component: PlanetComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
 import { PlanetListComponent } from './components/planet-list/planet-list.component';
 import { SearchComponent } from './components/search/search.component';
-import { HeroComponent } from './components/hero/hero.component'
+import { HeroComponent } from './components/hero/hero.component';
+import { PlanetComponent } from './pages/planet/planet.component';
+import { MainComponent } from './pages/main/main.component'
 
 @NgModule({
-  declarations: [AppComponent, PlanetListComponent, SearchComponent, HeroComponent],
+  declarations: [AppComponent, PlanetListComponent, SearchComponent, HeroComponent, PlanetComponent, MainComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
